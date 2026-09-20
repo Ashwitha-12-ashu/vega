952f5b4bffdaea3193594a6608b6b14010dd252a
ashwitha <kamaashu1234@gmail.com>
2026-09-20 15:07:42 +0530

    Add root URL

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -34,6 +34,7 @@ def api_root(request):
 
 
 urlpatterns = [
+    path('',api_root,name='root'),
     path('admin/', admin.site.urls),
     path('api/', api_root, name='api-root'),
     path('api/auth/', include('apps.accounts.urls')),
